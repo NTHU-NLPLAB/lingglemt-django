4cdc73b1578405f41a5cd636c7d3cbfa3660173b
dspp779 <dspp779@gmail.com>
2016-09-11 19:10:12 +0800

    fix bingslate not returning result after an error occurs

--- translator/bingslate.py
+++ translator/bingslate.py
@@ -28,26 +28,25 @@ class BingTranslator:
         data = json.dumps(data)
 
         result = None
-        while not result:
+        for _ in range(5):
             response = requests.post(url, headers=self.__header, data=data)
             # response = self.__session.post(url, cookies=self.__session.cookies, data=data)
             result = response.json()
             if response.status_code == 200 and 'items' in result:
-                for item in result['items']:
-                    yield item['text']
+                return [item['text'] for item in result['items']]
             else:
                 self.__on_session_expire__()
 
     def translate_one(self, sent):
         data = [{'text': sent}]
-        res = next(self.__translate__(data))
+        res = self.__translate__(data)[0]
         return res
 
     def translate_many(self, sents):
         res = []
         data = [{'text': sent} for sent in sents]
         for i in range(0, len(data), BATCH_SIZE):
-            res += list(self.__translate__(data[i:i + BATCH_SIZE]))
+            res += self.__translate__(data[i:i + BATCH_SIZE])
         return res
 
 
