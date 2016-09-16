17c5811fb38dc6c376de699ed9873d795b845ded
dspp779 <dspp779@gmail.com>
2016-09-16 22:55:34 +0800

    use environment varible of parser and models path

--- translator/settings.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# application settings
-STANFORD_PARSER_PATH = '/usr/local/Cellar/stanford-parser/3.6.0/libexec/stanford-parser.jar'
-STANFORD_PARSER_MODEL_PATH = '/usr/local/Cellar/stanford-parser/3.6.0/libexec/stanford-parser-3.6.0-models.jar'
